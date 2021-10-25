0d9d39ce9bcb632234237f7c89013ba3cd4ed063
Andrea Golden-Lasher <agolde15@jh.edu>
2021-10-25 14:36:29 -0600

    Added name field and game session

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
 let socket = io();
 let crazyButton = document.getElementById('crazyButton');
+let startButton = document.getElementById('startButton');
+let form = document.getElementById('nameForm');
+let input = document.getElementById('nameSubmit');
+let playerList = document.getElementById('players');
+let playerListDiv = document.getElementById('playerListDiv');
+let playerListHeading = document.getElementById('playerListHeading');
 
 startButton.addEventListener('click', () => {
     socket.emit('startGame');
@@ -12,9 +18,52 @@ crazyButton.addEventListener('click', () => {
     });
 })
 
+
+form.addEventListener('submit', sendGotNameMessage);
+
+//emit message when name is entered in form
+function sendGotNameMessage(e) {
+	e.preventDefault();
+	socket.emit('gotName', input.value);
+	form.style.display = 'none';
+	input.value = "";
+}
+
+// update list on client with player names who are waiting
+function updatePlayerList(names) {
+	
+	playerList.innerHTML = "";
+	names.forEach(function (name) {
+    	let li = document.createElement('li');
+	    li.innerText += name;
+		playerList.append(li);
+	});
+	playerListDiv.style.display = 'block';
+	
+}
+
+socket.on('connect', () => {
+	playerListDiv.style.display = 'none';
+	form.style.display = 'block';
+	playerListHeading.innerText = "Connected Players";
+});
+
+
+// update message to show that players have joined a game session
+socket.on('joinGame', (info) => {
+	playerListHeading.innerText = "Your opponents are: "+ info.names;
+	playerListDiv.style.display = 'none';
+
+});
+
+socket.on('updateWaitingList', (playerNames) => {
+	 updatePlayerList(playerNames);
+});
+
 socket.on('startGame', () => {
     hideStartButton();
 });
+
 socket.on('crazyIsClicked', (data) => {
     goCrazy(data.offsetLeft, data.offsetTop);
 });
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const uuidv4 = require("uuid/v4")
+
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
@@ -15,18 +17,79 @@ server.listen(port, ()=> {
     console.log(`Server is up on port ${port}.`)
 });
 
+//dictionary to store player data and game data
+var sessionData = {
+	"players": [],
+	"waitingPlayers": [],
+};
+
+//select first three players and make a game session for them
+function createGameInstance(){
+	let gameId = uuidv4();
+	sessionData[gameId] = []
+	for (var i = 0; i < 3; i++) {
+		
+		var player = sessionData["waitingPlayers"].shift();
+		sessionData["players"][player]["gameId"] = gameId;
+		sessionData[gameId].push(player);
+		//add player to game session room, remove from wait room
+	}
+	// make list of player names who are part of the game session
+	let gamePlayers = sessionData[gameId].map(tempId => {return sessionData["players"][tempId]["name"]});
+	sessionData[gameId].forEach(function (id) {
+		io.to(id).emit('joinGame', {"gameId":gameId, "names": gamePlayers});
+	})
+}
+
 io.on('connection', (socket) => {
-	console.log('A user just connected.');
-    socket.on('disconnect', () => {
-        console.log('A user has disconnected.');
-    })
+	console.log('A user just connected with ID ' + socket.id);
+    sessionData["players"][socket.id] = {"socket": socket, "name": "New Player", "gameId": null, "score": 0};
+
+	
+	socket.on('joinGame', (info) => {
+		socket.leave('waitingroom');
+		socket.join(info.gameId);		
+	});	
+
     socket.on('startGame', () => {
         io.emit('startGame');
     })
+
     socket.on('crazyIsClicked', (data) => {
         io.emit('crazyIsClicked', data);
     });
     
+	socket.on('gotName', (data) => {
+		sessionData["players"][socket.id]["name"] = data;
+		sessionData["waitingPlayers"].push(socket.id);
+		socket.join('waitingroom');
+		// make a game instance if there are more than three players
+		if (sessionData["waitingPlayers"].length >= 3) {
+			createGameInstance();
+		}
+		
+		// make list of player names who are in waiting room
+		let waitingPlayers = sessionData["waitingPlayers"].map(tempId => {return sessionData["players"][tempId]["name"]});
+		io.in("waitingroom").emit('updateWaitingList', waitingPlayers);
+	});
+	
+	socket.on('disconnect', () => {
+        console.log(sessionData["players"][socket.id]["name"] + " has disconnected.");
+		if (sessionData["players"][socket.id]["gameId"] != null){
+			console.log("player left the game");
+			let gameId = sessionData["players"][socket.id]["gameId"];
+			let pos = sessionData[gameId].indexOf(socket.id);
+			sessionData[gameId].splice(pos, 1);
+		} else {
+			let pos = sessionData["waitingPlayers"].indexOf(socket.id)
+			if (pos >= 0){
+				sessionData["waitingPlayers"].splice(pos, 1);
+			} 
+		}
+		
+		delete sessionData["players"][socket.id];
+		
+	})
 });
 
 
